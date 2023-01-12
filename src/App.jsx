65c4784470da807b954b2d04c4632af3a91f1877
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-01-12 11:47:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Components/Header/Header";
+import MemeGenerator from "./Components/MemeGenerator/MemeGenerator";
+
+export const App = () => {
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+};
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div>
+      <header className="header">
+        <img src="./images/troll-face.png" className="header--image" />
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">React Course - Project 3</h4>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
